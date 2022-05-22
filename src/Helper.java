5a3c2a1583e1350d4d20534ed4ce65d1aac43c25
Özgür Yaman <yamanozgur6333@gmail.com>
2022-05-22 15:55:20 +0300

    Girilen Sayidan buyuk en yakin sayi ve Girilen Sayidan kucuk en yakin sayi

--- src/Helper.java
+++ src/Helper.java
@@ -1,6 +1,6 @@
 public class Helper {
 
-    public static double harmonikHesap(int boyut){
+   /* public static double harmonikHesap(int boyut){
         double sum=0.0;
         int[] dizi=new int[boyut];
         for(int i=1;i<=boyut;i++){
@@ -11,5 +11,45 @@ public class Helper {
         }
         return sum;
 
+    }
+
+    */
+
+
+    public static void sayiBul(int[] arry, int sayi) {
+        int mini=0, maxi=0;
+        int sayac1=0,sayac2=0;
+        int[] kucukdizi = new int[5];
+        int[] buyukdizi = new int[3];
+        for (int i = 0; i < arry.length; i++) {
+            if (sayi < arry[i]) {
+
+                buyukdizi[sayac1] = arry[i];
+                sayac1++;
+            } else {
+                kucukdizi[sayac2] = arry[i];
+                sayac2++;
+            }
+
+        }
+        mini = buyukdizi[0];
+        for (int j = 0; j < buyukdizi.length; j++) {
+
+            if (mini > buyukdizi[j]) {
+                mini = buyukdizi[j];
+            }
+
+        }
+        maxi = kucukdizi[0];
+        for (int k = 0; k < kucukdizi.length; k++) {
+
+            if (maxi < kucukdizi[k]) {
+                maxi = kucukdizi[k];
+            }
+
+        }
+
+        System.out.println("Girilen Sayidan buyuk en yakin sayi :" + mini);
+        System.out.println("Girilen Sayidan kucuk en yakin sayi :" + maxi);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-
+/*
         Helper helper=new Helper();
         Scanner scan=new Scanner(System.in);
         System.out.print("Boyuta Giriniz :");
         int boyut=scan.nextInt();
         System.out.print("Harmonik Sayi :"+helper.harmonikHesap(boyut));
+
+ */
+
+        int[] dizi={15,12,788,1,-1,-778,2,0};
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Sayi :");
+        int sayi=sc.nextInt();
+        Helper helper=new Helper();
+        helper.sayiBul(dizi,sayi);
+
     }
 }
